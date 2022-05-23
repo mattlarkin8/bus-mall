b36f649b84ff0f70166d80389d9f300467ad8934
Matthew Larkin <matthewlarkin8@gmail.com>
2022-05-23 18:00:29 -0400

    Completed functionality for web page. Added Event Listeners and Handlers and renderImg function.

--- js/app.js
+++ js/app.js
@@ -2,9 +2,9 @@
 
 // *********** GLOBAL VARIABLES ****************
 
-let voteCount = 25;
+let voteCount = 5;
 let allProducts = [];
-let products = [['bag','jpg'],['banana','jpg'],['bathroom','jpg'],['boots','jpg'],['breakfast','jpg'],['bubblegum','jpg'],['chair','jpg'],['cthulu','jpg'],['dog-duck','jpg'],['dragon','jpg'],['pen','jpg'],['pet-sweep','jpg'],['scissors','jpg'],['shark','jpg'],['sweep','png'],['tauntaun','jpg'],['unicorn','jpg'],['water-can','jpg'],['wine-glass','jpg']];
+let products = [['bag','jpg'],['banana','jpg'],['bathroom','jpg'],['boots','jpg'],['breakfast','jpg'],['bubblegum','jpg'],['chair','jpg'],['cthulhu','jpg'],['dog-duck','jpg'],['dragon','jpg'],['pen','jpg'],['pet-sweep','jpg'],['scissors','jpg'],['shark','jpg'],['sweep','png'],['tauntaun','jpg'],['unicorn','jpg'],['water-can','jpg'],['wine-glass','jpg']];
 
 // ********** DOM REFERENCES *******************
 
@@ -41,4 +41,69 @@ function getRandomIndex(){
   return Math.floor(Math.random()*allProducts.length);
 }
 
+function renderImgs(){
+  let productOneIndex = getRandomIndex();
+  let productTwoIndex = getRandomIndex();
+  let productThreeIndex = getRandomIndex();
+
+  let i=0;
+  while(i===0){
+    if(productOneIndex === productTwoIndex || productOneIndex === productThreeIndex){
+      productOneIndex = getRandomIndex();
+    }else if(productTwoIndex === productThreeIndex){
+      productTwoIndex = getRandomIndex();
+    }else{i++;}
+  }
+
+  imgOne.src = allProducts[productOneIndex].img;
+  imgOne.alt = allProducts[productOneIndex].name;
+  allProducts[productOneIndex].views++;
+
+  imgTwo.src = allProducts[productTwoIndex].img;
+  imgTwo.alt = allProducts[productTwoIndex].name;
+  allProducts[productTwoIndex].views++;
+
+  imgThree.src = allProducts[productThreeIndex].img;
+  imgThree.alt = allProducts[productThreeIndex].name;
+  allProducts[productThreeIndex].views++;
+}
+
 instantiateProducts();
+renderImgs();
+
+// ********* EVENT HANDLERS *******************
+function handleClick(event) {
+  voteCount--;
+
+  let imgClicked = event.target.alt;
+
+  for(let i = 0; i < allProducts.length; i++){
+    if(imgClicked === allProducts[i].name){
+      allProducts[i].votes++;
+    }
+  }
+  //rerender 2 new Product images
+  renderImgs();
+
+  // once voting rounds completed - stop clicks
+  if(voteCount === 0){
+    imgContainer.removeEventListener('click', handleClick);
+
+  }
+
+}
+
+function handleShowResults(){
+  if(voteCount === 0){
+    for(let i = 0; i < allProducts.length; i++){
+      let liElem = document.createElement('li');
+      liElem.textContent = `${allProducts[i].name} was shown ${allProducts[i].views} times and voted for ${allProducts[i].votes} times.`;
+      resultsList.appendChild(liElem);
+    }
+  }
+}
+
+// ********* EVENT LISTENERS ******************
+
+imgContainer.addEventListener('click', handleClick);
+showResultsBtn.addEventListener('click', handleShowResults);
